540b257c44504c2c849a5cb16f7ca3032eac744b
shinmiji <911024s@naver.com>
2015-08-26 09:53:25 +0900

    browser-sync추가
    browser-sync추가

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,52 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade'); // jade 파일을 html 파일로 변경해 주는 일을 해준다.
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+reload = browserSync.reload;
 // html 대신에 jade를, css대신에 sass를 사용하는 개념.
 
 var config = {
-	'jade': {'pretty':true}
+	'jade': {'pretty':true},
+	'sass': {
+		'outputStyle': 'compact' // compact, compressed, nested, expanded
+	}
 };
 
-gulp.task('default', ['jade', 'sass', 'watch']);
+gulp.task('default', ['jade', 'sass'], function(){
+	browserSync({'server':'./dist'})
+	gulp.start('watch');
+});
 
 gulp.task('watch', function(){
-	gulp.watch(['src/index.jade'], ['jade']); // src/index.jade파일이 변경이 될 경우 gulp.task('jade', function(){ 부분을 실행하는것임
-	gulp.watch(['src/sass/style.scss'], ['sass']);
+	gulp.watch(['src/**/*.jade'], ['watch:jade']); // src/index.jade파일이 변경이 될 경우 gulp.task('jade', function(){ 부분을 실행하는것임
+	gulp.watch(['src/sass/**/*.scss'], ['sass']);
 })
+
+gulp.task('watch:jade', ['jade'], reload);
 
 gulp.task('jade', function(){
 	gulp.src('src/index.jade')
 		.pipe(jade(config.jade)) // 제작용일땐 pretty 옵션을 true로해서 압축x, 배포할 경우 pretty 옵션을 false로 해서 압축o
+		.on('error', errorLog)
 		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('sass', function(){
-	gulp.src('src/sass/style.scss')
-		.pipe(sass()) // 제작용일땐 pretty 옵션을 true로해서 압축x, 배포할 경우 pretty 옵션을 false로 해서 압축o
-		.pipe(gulp.dest('dist/css'));
+	gulp.src('src/sass/**/*.scss')
+		.pipe(sass(config.sass) // 제작용일땐 pretty 옵션을 true로해서 압축x, 배포할 경우 pretty 옵션을 false로 해서 압축o
+		.on('error', sass.logError))
+		.pipe(gulp.dest('dist/css'))
+		.pipe( reload({stream: true}) );
 });
+
+/**
+ * --------------------------------
+ * 유틸리티
+ * --------------------------------
+ */
+// 오류 출력을 위한 errorLog 함수
+// 오류 발생 시에도 watch 업무 중단하지 않음.
+function errorLog(error) {
+	console.error.bind(error);
+	this.emit('end');
+}
